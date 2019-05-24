d9461008ce25d8b9c96344046f2a976291efee30
ievgen degtiarenko <ievgen.degtiarenko@gmail.com>
2019-05-24 09:53:02 +0200

    fix compose version

--- install_dockercompose.sh
+++ install_dockercompose.sh
@@ -7,4 +7,6 @@ then
     version="1.24.0"
 fi
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+echo "Installing docker-compose $version"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/$version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
