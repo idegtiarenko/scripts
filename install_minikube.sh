94a89d0d93772ce50deaf58757b0ead271c6e963
ievgen degtiarenko <ievgen.degtiarenko@gmail.com>
2019-05-27 14:49:22 +0200

    install virtualbox for minikube usage

--- install_minikube.sh
+++ install_minikube.sh
@@ -7,4 +7,5 @@ then
     version="v1.1.0"
 fi
 
+sudo apt-get install virtualbox
 sudo curl -L "https://storage.googleapis.com/minikube/releases/$version/minikube-linux-amd64" -o /usr/local/bin/minikube
