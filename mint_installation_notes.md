5f936ed1598af15958a1aa1efa314edadfb51572
idegtiarenko <ievgen.degtiarenko@gmail.com>
2021-02-22 10:34:39 +0100

    mint post install configuration

---
+++ mint_installation_notes.md
@@ -0,0 +1,14 @@
+## Configure macro
+
+Using dconf-editor:
+* org/mate/macro/window-keybindings:
+  * begin-mode->disabled
+* org/mate/macro/global-keybindings:
+  * switch_to_workspace_up->disabled
+  * switch_to_workspace_down->disabled
+  * switch_to_workspace_left->disabled
+  * switch_to_workspace_right->disabled
+
+## Configure compiz
+
+TBD
---
+++ mint_post_install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "Removing unused software"
+
+sudo apt-get purge evolution-data-server evolution-data-server-common \
+                   gnote \
+                   drawing \
+                   hypnotix \
+                   pix pix-data \
+                   simple-scan \
+                   hexchat hexchat-common\
+                   transmission-gtk transmission-common \
+                   thunderbird \
+                   rhythmbox rhythmbox-data \
+                   libreoffice-draw libreoffice-math libreoffice-base \
+                   samba-common samba-common-bin \
+                   cups-server-common cups-common cups-daemon
+
+echo "Installing common software"
+sudo apt-get install git nmap traceroute htop
+
+echo "Installing mate configurers"
+sudo apt-get install dconf-editor mate-tweak compiz-plugins-extra
+
+echo "Installing gimp"
+sudo apt-get install gimp gimp-plugin-registry
+
+sudo apt-get install xplayer cheese
