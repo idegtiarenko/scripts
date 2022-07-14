18909ae654c350652b9e64cff0cc389deb111ca9
ievgen.degtiarenko <ievgen.degtiarenko@gmail.com>
2022-07-14 10:35:00 +0200

    update relevant java installation

--- install_java_tools.sh
+++ install_java_tools.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-sdk install java 8.0.242.hs-adpt
-sdk install java 11.0.6.hs-adpt
-sdk install java 14.0.0.hs-adpt
-sdk install maven 3.6.3
-sdk install gradle 6.3
+sdk install java 17.0.1-open
+sdk install gradle 7.4.2
 
-#sdk install scala 2.13.1
-#sdk install sbt 1.3.9
-
-#sdk install visualvm 2.0
+#sdk install visualvm 2.1.3
 
 sdk flush archives
